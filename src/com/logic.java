605986564be21259ea82590c198c92ee33137447
Nisansala-P <pcn.pathirane@gmail.com>
2022-05-22 21:22:29 +0200

    1st exercise

---
+++ src/com/logic.java
@@ -0,0 +1,18 @@
+package com;
+
+public class logic {
+	public void print() {
+		System.out.println("Hello");
+		System.out.println("Nisansala");
+	}
+
+	public int sum() {
+		
+		int i = 74;
+		int i2 = 36;
+		int sum = i+i2;
+		
+		System.out.println(sum);
+	return sum;	
+	}
+}
---
+++ src/com/runner.java
@@ -0,0 +1,11 @@
+package com;
+
+public class runner {
+	
+	public static void main(String args[]) {
+		logic myObj = new logic();
+		myObj.print();
+		myObj.sum();
+	}
+
+}
